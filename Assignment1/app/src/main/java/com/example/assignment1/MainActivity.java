cafc0841205910b1a575e99772ce6a267e6c91f6
chenerzhang <tuh12085@temple.edu>
2020-09-22 10:27:42 -0400

    + Fix the bug of null pointer + Complete the activity_main.xml basic UI setup

--- Assignment1/app/src/main/java/com/example/assignment1/MainActivity.java
+++ Assignment1/app/src/main/java/com/example/assignment1/MainActivity.java
@@ -4,12 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
+
+
 public class MainActivity extends AppCompatActivity {
+
+
+    Button start_button;
+    Button stop_button;
+    Button setting_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Init Buttons
+        button_init();
+
     }
+
+    public void button_init() {
+        start_button = (Button) findViewById(R.id.start_button);
+        stop_button = (Button) findViewById(R.id.stop_button);
+        setting_button = (Button) findViewById(R.id.setting_button);
+
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("start button trigger");
+            }
+        });
+
+        stop_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("stop button trigger");
+            }
+        });
+
+        setting_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("setting trigger");
+            }
+        });
+    }
+
+    ;
 }
