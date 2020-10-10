e4b64d655fa52bf2d1b4fd37041960d4b23b2892
chenerzhang <tuh12085@temple.edu>
2020-10-09 23:09:50 -0400

    + Format TracingFragment.java

--- Contract_Tracer/app/src/main/java/edu/temple/contacttracer/TracingFragment.java
+++ Contract_Tracer/app/src/main/java/edu/temple/contacttracer/TracingFragment.java
@@ -18,36 +18,24 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TracingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TracingFragment extends Fragment implements OnMapReadyCallback {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final int ZOOM = 15;
     GoogleMap my_map;
     MapView mapView;
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private double longtitude;
+    private double latitude;
+
 
     public TracingFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TracingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static TracingFragment newInstance(String param1, String param2) {
         TracingFragment fragment = new TracingFragment();
         Bundle args = new Bundle();
@@ -70,11 +58,24 @@ public class TracingFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        MarkerOptions markerOptions = new MarkerOptions();
+        my_map = googleMap;
+        LatLng latLng = new LatLng(longtitude, latitude);
+        markerOptions.position(latLng);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, ZOOM);
+        my_map.moveCamera(cameraUpdate);
+        my_map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        my_map.addMarker(markerOptions);
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            longtitude = getArguments().getDouble(ARG_PARAM1);
+            latitude = getArguments().getDouble(ARG_PARAM2);
         }
     }
 
@@ -83,18 +84,5 @@ public class TracingFragment extends Fragment implements OnMapReadyCallback {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tracing, container, false);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        MapsInitializer.initialize(getContext());
-        MarkerOptions markerOptions = new MarkerOptions();
-        my_map = googleMap;
-        LatLng latLng = new LatLng(39.981893, -75.148950);
-        markerOptions.position(latLng);
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 16);
-        my_map.moveCamera(cameraUpdate);
-        my_map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        my_map.addMarker(markerOptions);
     }
 }
